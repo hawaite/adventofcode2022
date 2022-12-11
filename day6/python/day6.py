a7560f335fc255cda2265dfddb12b0917a0b6ba8
Helier Waite <helierawaite@gmail.com>
2022-12-11 23:29:21 +0000

    day 6 part 2

--- day6/python/day6.py
+++ day6/python/day6.py
@@ -1,14 +1,18 @@
-def main():
+def main(windowSize):
     with open("../input.txt", "r") as fp:
         input = fp.readline();
-        windowStart, windowEnd = 0,3
+        windowStart, windowEnd = 0, windowSize
         while windowEnd < len(input):
-            if len(set(input[windowStart:windowEnd+1])) == 4:
+            if len(set(input[windowStart:windowEnd])) == windowSize:
                 break
             windowStart = windowStart + 1
             windowEnd = windowEnd + 1
 
-        print(f'found packet header at => ({windowStart}, {windowEnd+1})')
+        print(f'found packet header at => ({windowStart}, {windowEnd})')
+        print(f'result is {windowEnd}')
 
 if __name__ == "__main__":
-    main()
+    # part 1
+    main(4)
+    # part 2
+    main(14)
