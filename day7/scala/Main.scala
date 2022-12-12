bc29a83c7d18edf185eaeececbe94cd09906a1ff
Helier Waite <helierawaite@gmail.com>
2022-12-12 23:42:03 +0000

    refactoring out of the main method

--- day7/scala/Main.scala
+++ day7/scala/Main.scala
@@ -17,41 +17,41 @@ object FileSys{
     }
 
     class FileFsItem(name:String, size:Int) extends FsItem(name)
-    
+
     var root = new DirectoryFsItem("/", null)
     var pwd = root
 
-    def GetSubdirectoryFromCurrentDirectory(directoryName:String, pwd: DirectoryFsItem): Option[DirectoryFsItem] = {
-        var foundSubDirectory:Option[FsItem] = pwd.children.find((fsItem) => {
+    def GetSubdirectoryFromCurrentDirectory(directoryName:String): Option[DirectoryFsItem] = {
+        return this.pwd.children.find((fsItem) => {
             fsItem match{
                 case d: DirectoryFsItem => fsItem.getName().equals(directoryName)
                 case _ => false
             }
-        })
-
-        if(foundSubDirectory.isDefined){
-            return Some(foundSubDirectory.get.asInstanceOf[DirectoryFsItem])
-        }
-
-        return None
+        }).map(_.asInstanceOf[DirectoryFsItem])
     }
 
-    def GetFileFromCurrentDirectory(fileName:String, pwd: DirectoryFsItem): Option[FileFsItem] = {
-        var foundFile:Option[FsItem] = pwd.children.find((fsItem) => {
+    def GetFileFromCurrentDirectory(fileName:String): Option[FileFsItem] = {
+        this.pwd.children.find((fsItem) => {
             fsItem match{
                 case f: FileFsItem => fsItem.getName().equals(fileName)
                 case _ => false
             }
-        })
-
-        if(foundFile.isDefined){
-            return Some(foundFile.get.asInstanceOf[FileFsItem])
-        }
-
-        return None
+        }).map(_.asInstanceOf[FileFsItem])
     }
 
-    def parseInputLine(input:String) = {
+    def CreateFileInPwdIfDoesntExist(filename:String, filesize:Int) = {
+        if( GetFileFromCurrentDirectory(filename).isEmpty ){
+            this.pwd.children = this.pwd.children :+ new FileFsItem(filename, filesize)
+        }
+    }
+
+    def CreateSubdirectoryInPwdIfDoesntExist(directoryName:String) = {
+        if(GetSubdirectoryFromCurrentDirectory(directoryName).isEmpty){
+            this.pwd.children = this.pwd.children :+ new DirectoryFsItem(directoryName, this.pwd)
+        }
+    }
+
+    def ParseInputLine(input:String) = {
         var item = input.trim()
         // parse row in to tree structure
 
@@ -65,14 +65,12 @@ object FileSys{
         else if( item.startsWith("$ cd")){
             var dirName = item.drop(5)
             // change pwd to a child of current pwd with the name provided
-            GetSubdirectoryFromCurrentDirectory(dirName, this.pwd).foreach(foundDir => this.pwd = foundDir)
+            GetSubdirectoryFromCurrentDirectory(dirName).foreach(foundDir => this.pwd = foundDir)
         }
         else if( item.startsWith("dir ")){
             var dirName = item.drop(4)
             // add dir to pwd children if doesnt exist
-            if(GetSubdirectoryFromCurrentDirectory(dirName, this.pwd).isEmpty){
-                this.pwd.children = this.pwd.children :+ new DirectoryFsItem(dirName, this.pwd)
-            }
+            CreateSubdirectoryInPwdIfDoesntExist(dirName)
         }
         else if( item.startsWith("$ ls")){
             // does nothing
@@ -83,10 +81,7 @@ object FileSys{
             var fileRegex:Regex = """^(?<filesize>\d+) (?<filename>.*)$""".r
             item match {
                 case fileRegex(filesize, filename) => {
-                    var foundFile = GetFileFromCurrentDirectory(filename, this.pwd)
-                    if( foundFile.isEmpty ){
-                        this.pwd.children = this.pwd.children :+ new FileFsItem(filename, filesize.toInt)
-                    }
+                    CreateFileInPwdIfDoesntExist(filename, filesize.toInt)
                 }
                 case _ => throw new Exceptions.MalformedFileInputException
             }
@@ -97,9 +92,9 @@ object FileSys{
 object Main{
 
     def main(args: Array[String]):Unit = {
-        Source.fromFile("../input_custom.txt", "ascii")
+        Source.fromFile("../input.txt", "ascii")
             .getLines()
-            .foreach(FileSys.parseInputLine)
+            .foreach(FileSys.ParseInputLine)
         
         FileSys.root.children.foreach(x => println(x.getName()))
     }
