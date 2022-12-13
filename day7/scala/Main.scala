641af1d2be5a09743c4f0dbe0816081943060483
Helier Waite <helierawaite@gmail.com>
2022-12-13 01:08:47 +0000

    complete day 7 part 1

--- day7/scala/Main.scala
+++ day7/scala/Main.scala
@@ -14,14 +14,24 @@ object FileSys{
     class DirectoryFsItem(name:String, parent:DirectoryFsItem) extends FsItem(name){
         var children:Array[FsItem] = Array()
         def getParent() = parent
+        def getAbsolutePath():String = {
+            if(getName().equals("/"))
+                return ""
+            else{
+                return this.parent.getAbsolutePath() + "/" + getName()
+            }
+        }
     }
 
-    class FileFsItem(name:String, size:Int) extends FsItem(name)
+    class FileFsItem(name:String, size:Int) extends FsItem(name){
+        def getSize() = size
+    }
 
     var root = new DirectoryFsItem("/", null)
     var pwd = root
+    var filesizeTotal = 0
 
-    def GetSubdirectoryFromCurrentDirectory(directoryName:String): Option[DirectoryFsItem] = {
+    def getSubdirectoryFromCurrentDirectory(directoryName:String): Option[DirectoryFsItem] = {
         return this.pwd.children.find((fsItem) => {
             fsItem match{
                 case d: DirectoryFsItem => fsItem.getName().equals(directoryName)
@@ -30,7 +40,7 @@ object FileSys{
         }).map(_.asInstanceOf[DirectoryFsItem])
     }
 
-    def GetFileFromCurrentDirectory(fileName:String): Option[FileFsItem] = {
+    def getFileFromCurrentDirectory(fileName:String): Option[FileFsItem] = {
         this.pwd.children.find((fsItem) => {
             fsItem match{
                 case f: FileFsItem => fsItem.getName().equals(fileName)
@@ -39,19 +49,20 @@ object FileSys{
         }).map(_.asInstanceOf[FileFsItem])
     }
 
-    def CreateFileInPwdIfDoesntExist(filename:String, filesize:Int) = {
-        if( GetFileFromCurrentDirectory(filename).isEmpty ){
+    def createFileInPwdIfDoesntExist(filename:String, filesize:Int) = {
+        if( getFileFromCurrentDirectory(filename).isEmpty ){
             this.pwd.children = this.pwd.children :+ new FileFsItem(filename, filesize)
+            this.filesizeTotal += filesize
         }
     }
 
-    def CreateSubdirectoryInPwdIfDoesntExist(directoryName:String) = {
-        if(GetSubdirectoryFromCurrentDirectory(directoryName).isEmpty){
+    def createSubdirectoryInPwdIfDoesntExist(directoryName:String) = {
+        if(getSubdirectoryFromCurrentDirectory(directoryName).isEmpty){
             this.pwd.children = this.pwd.children :+ new DirectoryFsItem(directoryName, this.pwd)
         }
     }
 
-    def ParseInputLine(input:String) = {
+    def parseInputLine(input:String) = {
         var item = input.trim()
         // parse row in to tree structure
 
@@ -65,12 +76,12 @@ object FileSys{
         else if( item.startsWith("$ cd")){
             var dirName = item.drop(5)
             // change pwd to a child of current pwd with the name provided
-            GetSubdirectoryFromCurrentDirectory(dirName).foreach(foundDir => this.pwd = foundDir)
+            getSubdirectoryFromCurrentDirectory(dirName).foreach(foundDir => this.pwd = foundDir)
         }
         else if( item.startsWith("dir ")){
             var dirName = item.drop(4)
             // add dir to pwd children if doesnt exist
-            CreateSubdirectoryInPwdIfDoesntExist(dirName)
+            createSubdirectoryInPwdIfDoesntExist(dirName)
         }
         else if( item.startsWith("$ ls")){
             // does nothing
@@ -81,11 +92,53 @@ object FileSys{
             var fileRegex:Regex = """^(?<filesize>\d+) (?<filename>.*)$""".r
             item match {
                 case fileRegex(filesize, filename) => {
-                    CreateFileInPwdIfDoesntExist(filename, filesize.toInt)
+                    createFileInPwdIfDoesntExist(filename, filesize.toInt)
                 }
                 case _ => throw new Exceptions.MalformedFileInputException
             }
         }
+    }
+
+    def recursivelyFindLargeDirectoriesFromRoot() = {
+        // for each directory in the current directory
+        // calculate that directory size and sum it with the other directories
+        var directorySizes:collection.mutable.Map[String,Int] = collection.mutable.Map()
+        calculateDirectorySize(this.root, directorySizes)
+        println("result => " + directorySizes.filter((directoryTuple) => directoryTuple._2 <= 100000 ).map(_._2).reduce((x1, x2) => x1 + x2) )
+    }
+
+    def calculateDirectorySize(directory:DirectoryFsItem, directorySizes:collection.mutable.Map[String,Int]):Int = {
+
+        // sum all files in current directory
+        var currentDirectoryItemsSizes = directory
+            .children
+            .filter(_.isInstanceOf[FileFsItem])
+            .map(_.asInstanceOf[FileFsItem])
+            .map(x => x.getSize())
+
+        // guarding for empty directories
+        var currentDirectoryItemsSize = 0
+        if(!currentDirectoryItemsSizes.isEmpty){
+            currentDirectoryItemsSize = currentDirectoryItemsSizes.reduce((x1, x2) => x1 + x2)
+        }
+            
+        // recursively calculate the size of subdirectories
+        var currentDirectorySubdirectorySizes = directory
+            .children
+            .filter(_.isInstanceOf[DirectoryFsItem])
+            .map(_.asInstanceOf[DirectoryFsItem])
+            .map(x => calculateDirectorySize(x, directorySizes))
+
+        // guarding for not having any subdirectories
+        var currentDirectorySubdirectorySize = 0;
+        if(!currentDirectorySubdirectorySizes.isEmpty){
+            currentDirectorySubdirectorySize = currentDirectorySubdirectorySizes.reduce((x1, x2) => x1 + x2)
+        }
+            
+        var totalCurrentDirectorySize = currentDirectoryItemsSize + currentDirectorySubdirectorySize
+        directorySizes.addOne((directory.getAbsolutePath(), totalCurrentDirectorySize))
+
+        return totalCurrentDirectorySize
     }
 }
 
@@ -94,8 +147,8 @@ object Main{
     def main(args: Array[String]):Unit = {
         Source.fromFile("../input.txt", "ascii")
             .getLines()
-            .foreach(FileSys.ParseInputLine)
-        
-        FileSys.root.children.foreach(x => println(x.getName()))
+            .foreach(FileSys.parseInputLine)
+
+        FileSys.recursivelyFindLargeDirectoriesFromRoot()
     }
 }
