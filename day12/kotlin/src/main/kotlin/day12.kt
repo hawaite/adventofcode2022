8edeb9017d04b449af53055fffcbce7a24c0e244
Helier Waite <helierawaite@gmail.com>
2022-12-20 12:43:21 +0000

    added impossible routing checking and tidied day 12 further. part 1 still passes.

--- day12/kotlin/src/main/kotlin/day12.kt
+++ day12/kotlin/src/main/kotlin/day12.kt
@@ -30,68 +30,113 @@ class Node(x:Int, y:Int, height:Int ) {
     var visited: Boolean = false
 }
 
+class Graph{
+    val unvisited = ArrayList<Node>()
+    val nodeMap = HashMap<String,Node>()
+    private val compareByDistanceFromStartNode: Comparator<Node> = compareBy { it.shortestDistanceFromStartNode }
+    var startingNodeLabel:String = "";
+    var endingNodeLabel:String = "";
+    var rowMax = 0
+    var colMax = 0
+
+    fun parseFromLines(inputLines:List<String>,ignoreDefaultStartPosition:Boolean=false){
+        var col = 0
+        var row = 0
+
+        for ( line in inputLines ){
+            col = 0
+            for ( character in line){
+                var node = Node(col, row, charToHeight(character))
+    
+                if( character == 'S'){
+                    if(!ignoreDefaultStartPosition)
+                    node.shortestDistanceFromStartNode = 0
+                    this.startingNodeLabel = node.label
+                }
+                else if ( character == 'E'){
+                    this.endingNodeLabel = node.label
+                }
+                this.unvisited.add(node)
+                this.nodeMap.set(node.label, node)
+                col++
+            }
+            row++
+        }
+    
+        this.rowMax = row
+        this.colMax = col
+    }
+
+    fun resetGraph(){
+        // reset node values and the unvisited list
+        this.unvisited.clear()
+        for(node in nodeMap.values){
+            node.shortestDistanceFromStartNode = Int.MAX_VALUE
+            node.visited = false
+            this.unvisited.add(node)
+        }
+    }
+
+    fun setStartNode(nodeLabel:String){
+        this.startingNodeLabel = nodeLabel
+        this.nodeMap.get(this.startingNodeLabel)?.shortestDistanceFromStartNode = 0
+    }
+
+    fun calculateShortestPath():List<String>?{
+        // Perform Dijkstra to get shortest path to E node
+        while( ! this.unvisited.isEmpty() ){
+            this.unvisited.sortWith(compareByDistanceFromStartNode)
+            var currentNode = this.unvisited.removeAt(0)
+
+            if(currentNode.shortestDistanceFromStartNode == Int.MAX_VALUE){
+                // there were no more valid nodes. bail.
+                return null
+            }
+            
+            // process neighbours
+            if(currentNode.y != 0) // can test up
+                processAdjacentNode(currentNode, this.nodeMap.get(nodeLabelFromCoord(currentNode.x, currentNode.y - 1)))
+            if(currentNode.y != (this.rowMax - 1)) // can test down
+                processAdjacentNode(currentNode, this.nodeMap.get(nodeLabelFromCoord(currentNode.x, currentNode.y + 1)))
+            if(currentNode.x != 0) // can test left
+                processAdjacentNode(currentNode, this.nodeMap.get(nodeLabelFromCoord(currentNode.x - 1, currentNode.y)))
+            if(currentNode.x != (this.colMax - 1)) // can test right
+                processAdjacentNode(currentNode, this.nodeMap.get(nodeLabelFromCoord(currentNode.x + 1, currentNode.y)))
+
+            // add to visited list
+            currentNode.visited = true
+            if(currentNode.label == this.endingNodeLabel){
+                break
+            }
+        }
+        
+        var traceNode = this.nodeMap.get(this.endingNodeLabel)
+        var traceList = ArrayList<String>()
+
+        while(traceNode != null){
+            traceList.add(traceNode.label)
+            traceNode = this.nodeMap.get(traceNode.previous)
+        }
+
+        return traceList
+    }
+}
+
 fun main(args: Array<String>){
     val fileLines = File(args[0]).readLines()
 
-    val compareByDistanceFromStartNode: Comparator<Node> = compareBy { it.shortestDistanceFromStartNode }
+    var graph = Graph()
+    graph.parseFromLines(fileLines)
 
-    val unvisited = ArrayList<Node>()
-    val nodeMap = HashMap<String,Node>()
+    println("starting node: " + graph.startingNodeLabel)
+    println("  ending node: " + graph.endingNodeLabel)
 
-    var startingNodeLabel:String = "";
-    var endingNodeLabel:String = "";
+    val pathResult = graph.calculateShortestPath()
 
-    var row = 0
-    var col = 0
-
-    // read in characters to create nodes
-    for ( line in fileLines ){
-        col = 0
-        for ( character in line){
-            var node = Node(col, row, charToHeight(character))
-
-            if( character == 'S'){
-                node.shortestDistanceFromStartNode = 0
-                startingNodeLabel = node.label
-            }
-            else if ( character == 'E'){
-                endingNodeLabel = node.label
-            }
-            unvisited.add(node)
-            nodeMap.set(node.label, node)
-            col++
-        }
-        row++
+    if(pathResult == null){
+        println("No path between " + graph.startingNodeLabel + " and " + graph.endingNodeLabel)
     }
-
-    var rowMax = row
-    var colMax = col
-
-    println("starting node: " + startingNodeLabel)
-    println("  ending node: " + endingNodeLabel)
-
-    // Perform Dijkstra to get shortest path to E node
-    while( ! unvisited.isEmpty() ){
-        unvisited.sortWith(compareByDistanceFromStartNode)
-        var currentNode = unvisited.removeAt(0)
-
-        // process neighbours
-        if(currentNode.y != 0) // can test up
-            processAdjacentNode(currentNode, nodeMap.get(nodeLabelFromCoord(currentNode.x, currentNode.y - 1)))
-        if(currentNode.y != (rowMax - 1)) // can test down
-            processAdjacentNode(currentNode, nodeMap.get(nodeLabelFromCoord(currentNode.x, currentNode.y + 1)))
-        if(currentNode.x != 0) // can test left
-            processAdjacentNode(currentNode, nodeMap.get(nodeLabelFromCoord(currentNode.x - 1, currentNode.y)))
-        if(currentNode.x != (colMax - 1)) // can test right
-            processAdjacentNode(currentNode, nodeMap.get(nodeLabelFromCoord(currentNode.x + 1, currentNode.y)))
-
-        // add to visited list
-        currentNode.visited = true
-        if(currentNode.label == endingNodeLabel){
-            break
-        }
+    else{
+        println("ending distance from lst = " + (pathResult.size - 1))
     }
-
-    var currentTraceNode = nodeMap.get(endingNodeLabel)
-    println("ending distance = " + currentTraceNode?.shortestDistanceFromStartNode)
 }
