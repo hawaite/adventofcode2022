3b05341e940dedb7ec9a9b09a776b2cdebba553c
Helier Waite <helierawaite@gmail.com>
2022-12-21 00:35:02 +0000

    made day 13 output togglable. Going to leave it covered in print statements because I usually dont

--- day13/python/day13.py
+++ day13/python/day13.py
@@ -1,24 +1,21 @@
-from collections import namedtuple
 from enum import Enum
 
-# MessagePair = namedtuple("MessagePair", "left right")
-
+debug_out = False
 class RuleEvaluationResult(Enum):
     CORRECT_ORDER = 1
     INCORRECT_ORDER = 2
     CONTINUE = 3
 
-def parsePair(pairString):
-    splitPair = pairString.split('\n')
-
-    # return MessagePair(left=eval(splitPair[0]), right=eval(splitPair[1]))
+def debug(printStr):
+    if debug_out:
+        print(printStr)
 
 def testBothInt(left, right):
     if left < right:
-        print("left '" + str(left) + "' is less than right '" + str(right) + "' so correct order immediately")
+        debug("left '" + str(left) + "' is less than right '" + str(right) + "' so correct order immediately")
         return RuleEvaluationResult.CORRECT_ORDER
     if left > right:
-        print("left '" + str(left) + "' is more than right '" + str(right) + "' so wrong order immediately")
+        debug("left '" + str(left) + "' is more than right '" + str(right) + "' so wrong order immediately")
         return RuleEvaluationResult.INCORRECT_ORDER
     else:
         return RuleEvaluationResult.CONTINUE
@@ -41,12 +38,12 @@ def testBothLists(left, right):
             if currentResult != RuleEvaluationResult.CONTINUE:
                 return currentResult
     
-    # we're still on 'continue at this point'
+    # we're still on 'continue' at this point after running out of one of the lists
     if len(left) < len(right):
-        print("left ran out of items before right so correct order immediately")
+        debug("left ran out of items before right so correct order immediately")
         currentResult = RuleEvaluationResult.CORRECT_ORDER
     if len(left) > len(right):
-        print("right ran out of items before left so wrong order immediately")
+        debug("right ran out of items before left so wrong order immediately")
         currentResult = RuleEvaluationResult.INCORRECT_ORDER
 
     return currentResult
@@ -68,38 +65,41 @@ def testPairInCorrectOrder(left, right):
     i = 0
     while( currentResult == RuleEvaluationResult.CONTINUE and i < shortestListLength):
         if isinstance(left[i],int) and isinstance(right[i],int):
-            print("straight int compare")
-            print("compare " + str(left[i]) + " & " + str(right[i]))
+            debug("straight int compare")
+            debug("compare " + str(left[i]) + " & " + str(right[i]))
             currentResult = testBothInt(left[i], right[i])
-            print(currentResult)
-            print("----")
+            debug(currentResult)
+            debug("----")
         elif isinstance(left[i],list) and isinstance(right[i],list):
-            print("straight list compare")
-            print("compare " + str(left[i]) + " & " + str(right[i]))
+            debug("straight list compare")
+            debug("compare " + str(left[i]) + " & " + str(right[i]))
             currentResult = testBothLists(left[i], right[i])
-            print(currentResult)
-            print("----")
+            debug(currentResult)
+            debug("----")
         else:
             # some sort of mix
-            print("mixed input compare")
-            print("compare " + str(left[i]) + " & " + str(right[i]))
+            debug("mixed input compare")
+            debug("compare " + str(left[i]) + " & " + str(right[i]))
             currentResult = testListAndIntMix(left[i], right[i])
-            print(currentResult)
-            print("----")
+            debug(currentResult)
+            debug("----")
         i+=1
     
     if(currentResult == RuleEvaluationResult.CONTINUE):
-        print("ran out of pair-wise comparisons")
-        # either all items were the same, which should never happen
-        # of we ran out of items on one side
+        debug("ran out of pair-wise comparisons")
+        # either all items were the same, which I'm counting as correct ordering
+        # or we ran out of items on one side
         if len(left) < len(right):
-            print("left ran out of items before right so correct order immediately")
+            debug("left ran out of items before right so correct order immediately")
             currentResult = RuleEvaluationResult.CORRECT_ORDER
-        if len(left) > len(right):
-            print("right ran out of items before left so wrong order immediately")
+        elif len(left) > len(right):
+            debug("right ran out of items before left so wrong order immediately")
             currentResult = RuleEvaluationResult.INCORRECT_ORDER
+        else:
+            debug("the two items were equal. counting as correct")
+            currentResult = RuleEvaluationResult.CORRECT_ORDER
 
-    print("final result : " + str(currentResult))
+    debug("final result : " + str(currentResult))
     return currentResult
 
 def partOne(fileLines):
@@ -108,8 +108,8 @@ def partOne(fileLines):
     for ix, pair in enumerate(pairs):
         pairIndex = ix + 1
         splitPair = pair.split('\n')
-        print("==== pair "+str(pairIndex) + " ====")
-        print(splitPair)
+        debug("==== pair "+str(pairIndex) + " ====")
+        debug(splitPair)
         result = testPairInCorrectOrder(eval(splitPair[0]), eval(splitPair[1]))
         if(result == RuleEvaluationResult.CORRECT_ORDER):
             correct_index_sum += pairIndex
@@ -121,16 +121,16 @@ def partTwo(fileLines):
     filtered = list(filter(lambda x: x != '', pairs))
     filtered.append('[[2]]')
     filtered.append('[[6]]')
-    print(filtered)
+    debug(filtered)
     extremelyLazyInplaceKindOfBubbleSort(filtered)
-    print(filtered)
+    debug(filtered)
     overallResult = (filtered.index('[[6]]') + 1) * (filtered.index('[[2]]') + 1)
     print("overall result = " + str(overallResult))
 
 def extremelyLazyInplaceKindOfBubbleSort(items):
     isSorted = False
     while not isSorted :
-        isSorted = True #Assume true until proven otherwise
+        isSorted = True #Assume true until proven otherwise. Could be smarter.
         for i in range(0,len(items)-1):
             result = testPairInCorrectOrder(eval(items[i]), eval(items[i+1]))
             if result == RuleEvaluationResult.INCORRECT_ORDER:
@@ -139,7 +139,6 @@ def extremelyLazyInplaceKindOfBubbleSort(items):
                 items[i] = items[i+1]
                 items[i+1] = temp
                 isSorted = False
-
 
 def main():
     with open("../input.txt", "r") as fp:
