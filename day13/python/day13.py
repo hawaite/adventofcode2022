c8830de65848f12739b076485d843e7a4be26af7
Helier Waite <helierawaite@gmail.com>
2022-12-21 00:25:10 +0000

    day 13 part 2, featuring almost an actual sorting algorithm

--- day13/python/day13.py
+++ day13/python/day13.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from enum import Enum
 
-MessagePair = namedtuple("MessagePair", "left right")
+# MessagePair = namedtuple("MessagePair", "left right")
 
 class RuleEvaluationResult(Enum):
     CORRECT_ORDER = 1
@@ -10,7 +10,8 @@ class RuleEvaluationResult(Enum):
 
 def parsePair(pairString):
     splitPair = pairString.split('\n')
-    return MessagePair(left=eval(splitPair[0]), right=eval(splitPair[1]))
+
+    # return MessagePair(left=eval(splitPair[0]), right=eval(splitPair[1]))
 
 def testBothInt(left, right):
     if left < right:
@@ -61,28 +62,28 @@ def testListAndIntMix(left, right):
     return testBothLists(left_updated, right_updated)
 
 
-def testPairInCorrectOrder(pair:MessagePair):
+def testPairInCorrectOrder(left, right):
     currentResult = RuleEvaluationResult.CONTINUE
-    shortestListLength = min(len(pair.left), len(pair.right))
+    shortestListLength = min(len(left), len(right))
     i = 0
     while( currentResult == RuleEvaluationResult.CONTINUE and i < shortestListLength):
-        if isinstance(pair.left[i],int) and isinstance(pair.right[i],int):
+        if isinstance(left[i],int) and isinstance(right[i],int):
             print("straight int compare")
-            print("compare " + str(pair.left[i]) + " & " + str(pair.right[i]))
-            currentResult = testBothInt(pair.left[i], pair.right[i])
+            print("compare " + str(left[i]) + " & " + str(right[i]))
+            currentResult = testBothInt(left[i], right[i])
             print(currentResult)
             print("----")
-        elif isinstance(pair.left[i],list) and isinstance(pair.right[i],list):
+        elif isinstance(left[i],list) and isinstance(right[i],list):
             print("straight list compare")
-            print("compare " + str(pair.left[i]) + " & " + str(pair.right[i]))
-            currentResult = testBothLists(pair.left[i], pair.right[i])
+            print("compare " + str(left[i]) + " & " + str(right[i]))
+            currentResult = testBothLists(left[i], right[i])
             print(currentResult)
             print("----")
         else:
             # some sort of mix
             print("mixed input compare")
-            print("compare " + str(pair.left[i]) + " & " + str(pair.right[i]))
-            currentResult = testListAndIntMix(pair.left[i], pair.right[i])
+            print("compare " + str(left[i]) + " & " + str(right[i]))
+            currentResult = testListAndIntMix(left[i], right[i])
             print(currentResult)
             print("----")
         i+=1
@@ -91,31 +92,60 @@ def testPairInCorrectOrder(pair:MessagePair):
         print("ran out of pair-wise comparisons")
         # either all items were the same, which should never happen
         # of we ran out of items on one side
-        if len(pair.left) < len(pair.right):
+        if len(left) < len(right):
             print("left ran out of items before right so correct order immediately")
             currentResult = RuleEvaluationResult.CORRECT_ORDER
-        if len(pair.left) > len(pair.right):
+        if len(left) > len(right):
             print("right ran out of items before left so wrong order immediately")
             currentResult = RuleEvaluationResult.INCORRECT_ORDER
 
     print("final result : " + str(currentResult))
     return currentResult
 
+def partOne(fileLines):
+    pairs = fileLines.split('\n\n')
+    correct_index_sum = 0
+    for ix, pair in enumerate(pairs):
+        pairIndex = ix + 1
+        splitPair = pair.split('\n')
+        print("==== pair "+str(pairIndex) + " ====")
+        print(splitPair)
+        result = testPairInCorrectOrder(eval(splitPair[0]), eval(splitPair[1]))
+        if(result == RuleEvaluationResult.CORRECT_ORDER):
+            correct_index_sum += pairIndex
+
+    print("sum of correct indicies = " + str(correct_index_sum))
+
+def partTwo(fileLines):
+    pairs = fileLines.split('\n')
+    filtered = list(filter(lambda x: x != '', pairs))
+    filtered.append('[[2]]')
+    filtered.append('[[6]]')
+    print(filtered)
+    extremelyLazyInplaceKindOfBubbleSort(filtered)
+    print(filtered)
+    overallResult = (filtered.index('[[6]]') + 1) * (filtered.index('[[2]]') + 1)
+    print("overall result = " + str(overallResult))
+
+def extremelyLazyInplaceKindOfBubbleSort(items):
+    isSorted = False
+    while not isSorted :
+        isSorted = True #Assume true until proven otherwise
+        for i in range(0,len(items)-1):
+            result = testPairInCorrectOrder(eval(items[i]), eval(items[i+1]))
+            if result == RuleEvaluationResult.INCORRECT_ORDER:
+                # swap em
+                temp = items[i]
+                items[i] = items[i+1]
+                items[i+1] = temp
+                isSorted = False
+
+
 def main():
     with open("../input.txt", "r") as fp:
         allLines = fp.read()
-        pairs = allLines.split('\n\n')
-        correct_index_sum = 0
-        for ix, pair in enumerate(pairs):
-            pairIndex = ix + 1
-            parsedPair = parsePair(pair)
-            print("==== pair "+str(pairIndex) + " ====")
-            print(parsedPair)
-            result = testPairInCorrectOrder(parsedPair)
-            if(result == RuleEvaluationResult.CORRECT_ORDER):
-                correct_index_sum += pairIndex
-
-        print("sum of correct indicies = " + str(correct_index_sum))
+    #partOne(allLines)
+    partTwo(allLines)
 
 
 if __name__ == "__main__":
